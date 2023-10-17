35b40ce599b419920f4cb6c5a51f5780a7cb17be
buezrico <ricobuez@gmail.com>
2023-10-17 06:18:00 +0100

    fixed the hydration error bug

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -25,7 +25,8 @@ export default function Header({}: Props) {
         transition={{
           duration: 1.5,
         }}
-        className="flex flex-row items-center">
+        className="flex flex-row items-center"
+      >
         {/*social icons*/}
         <SocialIcon
           url="https://www.twitter.com/olapaulsunkanmi"
@@ -45,33 +46,35 @@ export default function Header({}: Props) {
         />
       </motion.div>
 
-      <Link href="#Contact">
-        <motion.div
-          initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer ">
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            bgColor="transparent"
-            fgColor="gray"
-          />
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer "
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          bgColor="transparent"
+          fgColor="gray"
+          url="#Contact"
+        />
+        <Link href="#Contact">
           <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
             get in touch
           </p>
-        </motion.div>
-      </Link>
+        </Link>
+      </motion.div>
     </header>
   );
 }
